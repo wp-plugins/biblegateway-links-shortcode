248352c4a19f4099ed26fe2671a7e1d5aaa5d3bd
jtsternberg <jtsternberg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-16 02:03:58 +0000

    ### 0.1.5 ### * Bug fixes, and filters available for each option.
    git-svn-id: https://plugins.svn.wordpress.org/biblegateway-links-shortcode/trunk@839384 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- biblegateway-search-shortcode.php
+++ biblegateway-search-shortcode.php
@@ -6,7 +6,7 @@ Description: Shortcode for linking Bible references to a BibleGateway page. Link
 Author URI: http://dsgnwrks.pro
 Author: Justin Sternberg
 Donate link: http://dsgnwrks.pro/give/
-Version: 0.1.4
+Version: 0.1.5
 */
 
 class DsgnWrks_Bible_Gateway_Shortcode {
